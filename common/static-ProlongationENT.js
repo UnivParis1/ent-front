50698826169cc9abdce13ae2559fb52aa3c15a34
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2016-01-18 11:01:44 +0100

    accueil: handle iframe, but without window.parent.up.userAttributes (since we mix esup.univ-paris1.fr and ent.univ-paris1.fr)

--- common/static-ProlongationENT.js
+++ common/static-ProlongationENT.js
@@ -2,4 +2,8 @@ var IMPERSONATE_COOKIE = 'CAS_TEST_IMPERSONATE';
 if (m = document.cookie.match(new RegExp(IMPERSONATE_COOKIE + "=(\\w+)"))) {
     window.bandeau_ENT.uid = m[1];
 }
-document.write("<script src='https://esup-test.univ-paris1.fr/ProlongationENT/loader.js'></script>\n");
+if (parent == window) {
+    document.write("<script src='https://esup-test.univ-paris1.fr/ProlongationENT/loader.js'></script>\n");
+} else {
+    document.write("<script src='https://esup-data.univ-paris1.fr/esup/outils/postMessage-resize-iframe-in-parent.js'></script>\n");
+}
