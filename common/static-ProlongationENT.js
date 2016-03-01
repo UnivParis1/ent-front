2cf3d3f11da58d23c0452673b79fee14c6f5a60d
Pascal Rigaux <Pascal.Rigaux@univ-paris1.fr>
2016-03-01 16:30:36 +0100

    use loader.js from servlet ProlongationENT

--- common/static-ProlongationENT.js
+++ common/static-ProlongationENT.js
@@ -3,7 +3,7 @@ if (m = document.cookie.match(new RegExp(IMPERSONATE_COOKIE + "=(\\w+)"))) {
     window.bandeau_ENT.uid = m[1];
 }
 if (parent == window) {
-    document.write("<script src='https://esup-test.univ-paris1.fr/ProlongationENT/loader.js'></script>\n");
+    document.write("<script src='https://ent-test.univ-paris1.fr/ProlongationENT/loader.js'></script>\n");
 } else {
     document.write("<script src='https://esup-data.univ-paris1.fr/esup/outils/postMessage-resize-iframe-in-parent.js'></script>\n");
 }
