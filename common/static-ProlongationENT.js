99967cc1b815a3a440f63a3225dc8ad57194fbef
Pascal Rigaux <Pascal.Rigaux@univ-paris1.fr>
2016-03-03 16:37:25 +0100

    ensure accueil in iframe does not go mad

--- common/static-ProlongationENT.js
+++ common/static-ProlongationENT.js
@@ -5,5 +5,6 @@ if (m = document.cookie.match(new RegExp(IMPERSONATE_COOKIE + "=(\\w+)"))) {
 if (parent == window) {
     document.write("<script src='https://ent-test.univ-paris1.fr/ProlongationENT/loader.js'></script>\n");
 } else {
+    window.resizeIframe_disableDangerousWindowResize = true;
     document.write("<script src='https://esup-data.univ-paris1.fr/esup/outils/postMessage-resize-iframe-in-parent.js'></script>\n");
 }
