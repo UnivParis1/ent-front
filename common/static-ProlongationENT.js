967f18b43f032dd4bf1fba9eaded17228b874742
Pascal Rigaux <Pascal.Rigaux@univ-paris1.fr>
2016-09-15 15:47:29 +0200

    simplify access to ProlongationENT/loader.js

--- common/static-ProlongationENT.js
+++ common/static-ProlongationENT.js
@@ -3,5 +3,5 @@ if (m = document.cookie.match(new RegExp(IMPERSONATE_COOKIE + "=(\\w+)"))) {
     window.bandeau_ENT.uid = m[1];
 }
 if (parent == window) {
-    document.write("<script src='https://ent-test.univ-paris1.fr/ProlongationENT/loader.js'></script>\n");
+    document.write("<script src='/ProlongationENT/loader.js'></script>\n");
 }
