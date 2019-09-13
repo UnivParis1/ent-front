695c4fbefa52eca632eb3fedf4c6808d41c074b3
Pascal Rigaux <Pascal.Rigaux@univ-paris1.fr>
2019-09-13 12:24:18 +0200

    epi & epi-ens: not used anymore. for compatibility, we now redirect directly to cours.univ-paris1.fr

--- common/static-ProlongationENT.js
+++
@@ -1,7 +0,0 @@
-var IMPERSONATE_COOKIE = 'CAS_TEST_IMPERSONATE';
-if (m = document.cookie.match(new RegExp(IMPERSONATE_COOKIE + "=(\\w+)"))) {
-    window.bandeau_ENT.uid = m[1];
-}
-if (parent == window) {
-    document.write("<script src='/ProlongationENT/loader.js'></script>\n");
-}
